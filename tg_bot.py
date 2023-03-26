4e9b6488fa3cfc146ae6316704a1846b0d04b541
Anton Zamyatin <zamyatin.anton@gmail.com>
2023-03-26 21:40:43 +0300

    fix mrkdwn escapes

--- tg_bot.py
+++ tg_bot.py
@@ -15,7 +15,7 @@ logging.basicConfig(
 
 def escape_markdown(text: str) -> str:
     """Escape characters in the given text that have special meaning in Markdown."""
-    escape_chars = r'-|.!'
+    escape_chars = r'()-|.!'
     return re.sub(r'([{}])'.format(re.escape(escape_chars)), r'\\\1', text)
 
 
